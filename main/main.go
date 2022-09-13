17bc8c31ecdf69c5d85b0f571107d09335ece48b
PontusNorrby <pontus.norrby@hotmail.se>
2022-09-13 14:53:28 +0200

    Self Node changes
    Added changes to Main that starts creates own routing table

--- main/main.go
+++ main/main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/PontusNorrby/D7024E-Kademlia"
+	d7024e "github.com/PontusNorrby/D7024E-Kademlia"
+	"log"
+	"net"
+)
+
+var (
+	BaseIp string = "172.20.128.2"
 )
 
 func main() {
-	//rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
-	rt := d7024e.NewRoutingTable(d7024e.NewContact(d7024e.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
-
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8003"))
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111300000000000000000000000000000000"), "localhost:8004"))
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("1111111400000000000000000000000000000000"), "localhost:8005"))
-	rt.AddContact(d7024e.NewContact(d7024e.NewKademliaID("2111111400000000000000000000000000000000"), "localhost:8006"))
-
-	contacts := rt.FindClosestContacts(d7024e.NewKademliaID("2111111400000000000000000000000000000000"), 20)
-	for i := range contacts {
-		fmt.Println(contacts[i].String())
+	id := d7024e.NewRandomKademliaID()
+	LocalIp := GetOutboundIP()
+	selfContact := d7024e.NewContact(id, LocalIp.String())
+	newRouteTable := d7024e.NewRoutingTable(selfContact)
+	if selfContact.String() == BaseIp {
+		//First node in the network
 	}
+	//A new node in the network
+	//func join network..
+	fmt.Printf("", newRouteTable)
+}
 
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
