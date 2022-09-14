95424c63755b3de65077a10afa22016649105d6a
Zoupi1 <47620170+Zoupi1@users.noreply.github.com>
2022-09-14 15:27:25 +0200

    Kademlia and Network struct

--- network.go
+++ network.go
@@ -1,6 +1,17 @@
 package d7024e
 
 type Network struct {
+	Kademlia     *Kademlia
+	MyNode       *Contact
+	RoutingTable *RoutingTable
+}
+
+func NewNetwork(Kademlia *Kademlia, MyNode *Contact, rt *RoutingTable) Network {
+	network := Network{}
+	network.Kademlia = Kademlia
+	network.RoutingTable = rt
+	network.MyNode = MyNode
+	return network
 }
 
 func Listen(ip string, port int) {
