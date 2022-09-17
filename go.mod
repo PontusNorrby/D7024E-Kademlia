755d3e1d2fbbe7cf75fcbbb0dd620eb6b104fa6d
noorbhtiti <nour.bahtite@gmail.com>
2022-09-17 14:07:32 +0200

    restructuring

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/PontusNorrby/D7024E-Kademlia
-
-go 1.19
--- src/routing/bucket.go
+++ src/routing/bucket.go
@@ -1,7 +1,8 @@
-package d7024e
+package routing
 
 import (
 	"container/list"
+	"github.com/PontusNorrby/D7024E-Kademlia/src/kademlia"
 )
 
 // bucket definition
@@ -19,10 +20,10 @@ func newBucket() *bucket {
 
 // AddContact adds the Contact to the front of the bucket
 // or moves it to the front of the bucket if it already existed
-func (bucket *bucket) AddContact(contact Contact) {
+func (bucket *bucket) AddContact(contact kademlia.Contact) {
 	var element *list.Element
 	for e := bucket.list.Front(); e != nil; e = e.Next() {
-		nodeID := e.Value.(Contact).ID
+		nodeID := e.Value.(kademlia.Contact).ID
 
 		if (contact).ID.Equals(nodeID) {
 			element = e
@@ -38,13 +39,13 @@ func (bucket *bucket) AddContact(contact Contact) {
 	}
 }
 
-// GetContactAndCalcDistance returns an array of Contacts where 
+// GetContactAndCalcDistance returns an array of Contacts where
 // the distance has already been calculated
-func (bucket *bucket) GetContactAndCalcDistance(target *KademliaID) []Contact {
-	var contacts []Contact
+func (bucket *bucket) GetContactAndCalcDistance(target *kademlia.KademliaID) []kademlia.Contact {
+	var contacts []kademlia.Contact
 
 	for elt := bucket.list.Front(); elt != nil; elt = elt.Next() {
-		contact := elt.Value.(Contact)
+		contact := elt.Value.(kademlia.Contact)
 		contact.CalcDistance(target)
 		contacts = append(contacts, contact)
 	}
