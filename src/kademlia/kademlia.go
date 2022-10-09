db2bb9cd7071afaad58a6d406b8e9640a70c145c
PontusNorrby <pontus.norrby@hotmail.se>
2022-10-09 22:10:12 +0200

    Updated Kademlia

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -18,25 +18,59 @@ func NewKademliaStruct(network *Network) *Kademlia {
 	return kademlia
 }
 
-func (kademlia *Kademlia) LookupContact(target *KademliaID) {
-	// TODO
+func (kademlia *Kademlia) LookupContact(target *KademliaID) ContactCandidates {
 	closerContacts := kademlia.Network.RoutingTable.FindClosestContacts(target, bucketSize)
+	contactCandidates := kademlia.lookupContactTest(target, closerContacts)
+	nodeContacts := contactCandidates.contacts
+	if target.Equals(kademlia.Network.RoutingTable.me.ID) || 20 > len(nodeContacts) {
+		contact := kademlia.Network.RoutingTable.me
+		contact.CalcDistance(target)
+		nodeContacts = append([]Contact{contact}, nodeContacts...)
+		contactCandidates := ContactCandidates{nodeContacts}
+		contactCandidates.Sort()
+		return contactCandidates
+	}
+	return contactCandidates
+}
+
+func (kademlia *Kademlia) lookupContactTest(target *KademliaID, earlierContacts []Contact) ContactCandidates {
+	foundContacts := 0
 	newRouteFinding := NewRoutingTable(*kademlia.Network.CurrentNode)
-	minLength := minVal(alphaValue, len(closerContacts))
+	minLength := minVal(alphaValue, len(earlierContacts))
 	for i := 0; i < minLength; i++ {
-		newContact := closerContacts[i]
-		contactsFetched := kademlia.Network.SendFindContactMessage(&newContact, target)
-		for _, tempContact := range contactsFetched {
-			newRouteFinding.AddContact(tempContact)
+		newContact := earlierContacts[i]
+		go func(newContact Contact) {
+			contactsFetched := kademlia.Network.SendFindContactMessage(&newContact, target)
+			for _, middleContact := range contactsFetched {
+				newRouteFinding.AddContact(middleContact)
+			}
+		}(newContact)
+	}
+	contactClosest := newRouteFinding.FindClosestContacts(target, bucketSize)
+	for _, contact := range contactClosest {
+		for _, previousContact := range earlierContacts {
+			if contact.ID.Equals(previousContact.ID) {
+				foundContacts += 1
+				break
+			}
 		}
 	}
-
+	if foundContacts == len(contactClosest) {
+		return ContactCandidates{contactClosest}
+	} else {
+		return kademlia.lookupContactTest(target, contactClosest)
+	}
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
 	// TODO
 }
 
+func (kademlia *Kademlia) storeHelp(data []byte) ([]*KademliaID, string) {
+
+}
+
+//Just stores the data in this node not on the "correct" node
 func (kademlia *Kademlia) Store(data []byte) KademliaID {
 	storeId := NewKademliaID(string(data))
 	dataStore := Value{data: data}
